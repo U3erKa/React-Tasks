5505d396e0e2097ca6f4ba5c34aa07b6d12eef46
Viktor Stepanov <vitya.zap@gmail.com>
2022-11-12 19:37:36 +0200

    Add lap button

--- src/components/Stopwatch/index.jsx
+++ src/components/Stopwatch/index.jsx
@@ -7,6 +7,7 @@ class Stopwatch extends Component {
     this.state = {
       seconds: 0,
       isStarted: false,
+      laps: [],
     };
   }
 
@@ -20,25 +21,42 @@ class Stopwatch extends Component {
     clearInterval(this.intervalId);
     this.setState({ isStarted: false });
   };
+  addLap = () => {
+    const { laps, seconds } = this.state;
+    this.setState({ laps: [...laps, seconds] });
+  };
   stopTimer = () => {
     this.pauseTimer();
-    this.setState({ seconds: 0 });
+    this.setState({ seconds: 0, laps: [] });
   };
 
-  componentDidMount() {}
+  lapsList = () =>
+    this.state.laps.map((time, id) => {
+      return (
+        <li key={id} className={styles.lap}>
+          {id}: {time}
+        </li>
+      );
+    });
 
   render() {
     const { seconds, isStarted } = this.state;
-    const { container, time, btn, startBtn, pauseBtn, stopBtn } = styles;
+    const { container, time, btn, startBtn, pauseBtn, lapBtn, stopBtn } = styles;
+    const laps = this.lapsList();
 
     return (
       <article className={container}>
         <p className={time}>Time: {seconds}</p>
+        <p>Laps:</p>
+        <ul>{laps}</ul>
         <button
           className={`${isStarted ? pauseBtn : startBtn} ${btn}`}
           onClick={isStarted ? this.pauseTimer : this.startTimer}
         >
           {isStarted ? 'Pause' : 'Start'}
+        </button>
+        <button className={`${lapBtn} ${btn}`} onClick={this.addLap}>
+          Add lap
         </button>
         <button className={`${stopBtn} ${btn}`} onClick={this.stopTimer}>
           Stop
