2c69dc9187831cb07334fcceb0ae1df8d4416d0a
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-03 23:09:43 +0200

    Add styles
    Minor form improvements

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,28 +1,50 @@
-import { ChangeEvent, FormEvent, useReducer } from 'react';
+import { ChangeEvent, FormEvent, useCallback, useReducer } from 'react';
 import action, { reducer, initialState } from './reducer';
+import styles from './Todo.module.scss';
+import checkOutline from './check-outline.svg';
+import checkBold from './check-bold.svg';
+import deleteOutline from './delete-outline.svg';
 
 export default function Todo() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const mapTodos = () =>
+  const mapTodos = useCallback(() =>
     state.todos.map(({ text, isDone }, i) => (
-      <li key={i}>
-        <p>{text}</p>
-        <button onClick={() => dispatch(action.actMarkDone(i))}>Mark as {isDone && 'un'}done</button>
-        <button onClick={() => dispatch(action.actDelete(i))}>Delete</button>
+      <li className={styles.todoListItem} key={i}>
+        <p className={styles.todoListItemText}>{text}</p>
+        <button className={styles.todoListBtn} onClick={() => dispatch(action.actMarkDone(i))}>
+          <img className={styles.todoListBtnIcon} src={isDone ? `${checkBold}` : `${checkOutline}`} alt={`Mark as ${isDone ? '' : 'un'}done`} />
+        </button>
+        <button className={styles.todoListBtn} onClick={() => dispatch(action.actDelete(i))}>
+          <img className={styles.todoListBtnIcon} src={deleteOutline} alt={'Delete'} />
+        </button>
       </li>
-    ));
+    )), [state.todos]);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(state);
+  };
 
   return (
-    <div>
-      <input
-        name="todo"
-        value={state.todo}
-        onChange={({ target: { value } }: ChangeEvent<HTMLInputElement>) => dispatch(action.actText(value))}
-      />
-      <button onClick={(e: FormEvent<HTMLButtonElement>) => dispatch(action.actTodo())}>Add</button>
-      <button onClick={(e: FormEvent<HTMLButtonElement>) => dispatch(action.actReset())}>Reset</button>
-      <ul>{mapTodos()}</ul>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <fieldset className={styles.fieldset}>
+          <textarea
+            className={styles.todoInput}
+            name="todo"
+            value={state.todo}
+            onChange={({ target: { value } }: ChangeEvent<HTMLTextAreaElement>) => dispatch(action.actText(value))}
+          />
+          <button className={styles.todoBtn} onClick={(e: FormEvent<HTMLButtonElement>) => dispatch(action.actTodo())}>
+            Add
+          </button>
+          <button className={styles.todoBtn} onClick={(e: FormEvent<HTMLButtonElement>) => dispatch(action.actReset())}>
+            Reset
+          </button>
+        </fieldset>
+        <ul className={styles.todoList}>{mapTodos()}</ul>
+      </form>
     </div>
   );
 }
